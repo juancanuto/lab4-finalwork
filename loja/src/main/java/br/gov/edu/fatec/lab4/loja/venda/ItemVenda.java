13974a61d9b514d1f35e9f04c6d25cb24e8a2724
André Maurílio <andrehakon@gmail.com>
2018-12-07 14:01:00 -0200

    Refatoração de código para que a aplicação subisse com sucesso, e também executando o liquibase com sucesso.

--- loja/src/main/java/br/gov/edu/fatec/lab4/loja/venda/ItemVenda.java
+++ loja/src/main/java/br/gov/edu/fatec/lab4/loja/venda/ItemVenda.java
@@ -1,24 +1,33 @@
 package br.gov.edu.fatec.lab4.loja.venda;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import br.gov.edu.fatec.lab4.loja.produto.Produto;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name="item_venda")
 @Getter
 @Setter
-public class ItemVenda {
+public class ItemVenda implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7081738732278992122L;
+	@Id
 	@ManyToOne
 	@JoinColumn(name = "produto_id")
 	Produto produto;
-	
+	@Id
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="venda_id")
 	Venda venda;
